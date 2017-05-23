146367f723811f6069379a84957f1ce5c98278ba
Jonathan <jmartinez@cymsuroeste.com>
2017-05-23 18:17:17 +0200

    applying responsive & mobile first, adding react-bootstrap tooltips

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import Navbar from './Navbar.jsx';
 import Particles from 'react-particles-js';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
+import {OverlayTrigger, Button, Tooltip} from 'react-bootstrap';
 
+export default class App extends Component {
+  render() {
     const particlesOptions = {
       particles: {
         "number":{
@@ -15,8 +15,15 @@ export default class App extends Component {
             "value_area": 800
           }
         },
+        "color":{
+          "value": "#343d46",
+        },
+        "line_linked":{
+          "color": "#343d46"
+        }
       },
       interactivity:{
+        "detect_on": "window",
         "events":{
           "onhover":{
             "enable":true,
@@ -30,21 +37,50 @@ export default class App extends Component {
       },
       retina_detect: false
     }
-  }
-  render() {
+
+    const tooltipGithub = (
+      <Tooltip id="tooltip">@jonathanmartinez</Tooltip>
+    );
+    const tooltipLinkedin = (
+      <Tooltip id="tooltip">/jonathan-martinez</Tooltip>
+    );
+    const tooltipEmail = (
+      <Tooltip id="tooltip">jonymartinez1@gmail.com</Tooltip>
+    );
+    const tooltipSkype = (
+      <Tooltip id="tooltip">@skype</Tooltip>
+    );
+
     return (
       <div className="App">
-        <Particles params={this.particlesOptions} />
+        <Particles params={particlesOptions} />
         <Navbar />
         <main className="container">
           <section className="row">
             <article className="col-xs-10 col-xs-offset-1 text-center">
               <h1 className="name">LOREMEMM IPSUMMM</h1>
-              <p className="lead">LOREMMM IPSUMNNN</p>
-              <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.</p>
-              <p>Lorem Ipsum es simplemente <a href="#">el texto</a> de relleno de las imprentas y archivos de texto. .</p>
-              <a className="social-link" href="#"> <i className="fa fa-github"></i> Github </a>
-              <a className="social-link" href="#"> <i className="fa fa-linkedin"></i> Linkedin </a>
+              <p className="lead">Software Engineer</p>
+              <a className="tag-link" href="https://developer.mozilla.org/en/docs/Web/JavaScript">#js</a>
+              <a className="tag-link" href="https://facebook.github.io/react/">#reactjs</a>
+              <a className="tag-link" href="https://angular.io/">#angular</a>
+              <a className="tag-link" href="http://php.net/">#php</a>
+              <a className="tag-link" href="https://nodejs.org/">#nodejs</a>
+              <a className="tag-link" href="https://www.mysql.com/">#mysql</a>
+              <a className="tag-link" href="https://www.mongodb.com/">#mongodb</a>
+              <p className="bio">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.</p>
+              <p className="last-project">Lorem Ipsum es simplemente <a href="#">el texto</a> de relleno de las imprentas y archivos de texto. .</p>
+              <OverlayTrigger placement="top" overlay={tooltipGithub}>
+                <a className="social-link" href="#"><i className="fa fa-github right-10"></i></a>
+              </OverlayTrigger>
+              <OverlayTrigger placement="top" overlay={tooltipLinkedin}>
+                <a className="social-link" href="#"><i className="fa fa-linkedin right-10"></i></a>
+              </OverlayTrigger>
+              <OverlayTrigger placement="top" overlay={tooltipEmail}>
+                <a className="social-link" href="#"><i className="fa fa-envelope right-10"></i></a>
+              </OverlayTrigger>
+              <OverlayTrigger placement="top" overlay={tooltipSkype}>
+                <a className="social-link" href="#"><i className="fa fa-skype right-10"></i></a>
+              </OverlayTrigger>
             </article>
           </section>
         </main>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from '../images/logo.png';
+import logoBlue from '../images/logo_blue.png';
 
 export default class Navbar extends Component {
   render() {
@@ -7,7 +8,7 @@ export default class Navbar extends Component {
       <nav className="container">
         <div className="row">
           <div className="col-xs-12">
-            <img height="50" className="logo" src={logo} alt="Logo JM" />
+            <img height="50" className="logo" src={logoBlue} alt="Logo JM" />
           </div>
         </div>
       </nav>
