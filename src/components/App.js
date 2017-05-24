8439e3f362d61640b5a91a5a78e29bd3704cd1ea
Jonathan <jmartinez@cymsuroeste.com>
2017-05-24 12:25:59 +0200

    adding title, descripcion and texts

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar.jsx';
 import Particles from 'react-particles-js';
-
 import {OverlayTrigger, Button, Tooltip} from 'react-bootstrap';
 
 export default class App extends Component {
@@ -42,7 +41,7 @@ export default class App extends Component {
       <Tooltip id="tooltip">@jonathanmartinez</Tooltip>
     );
     const tooltipLinkedin = (
-      <Tooltip id="tooltip">/jonathan-martinez</Tooltip>
+      <Tooltip id="tooltip">/jonathan-martinez1</Tooltip>
     );
     const tooltipEmail = (
       <Tooltip id="tooltip">jonymartinez1@gmail.com</Tooltip>
@@ -57,30 +56,46 @@ export default class App extends Component {
         <Navbar />
         <main className="container">
           <section className="row">
-            <article className="col-xs-10 col-xs-offset-1 text-center">
-              <h1 className="name">LOREMEMM IPSUMMM</h1>
+            <article className="col-xs-12 col-sm-10 col-sm-offset-1 text-center">
+              <h1 className="name">JONATHAN MARTÍNEZ</h1>
               <p className="lead">Software Engineer</p>
-              <a className="tag-link" href="https://developer.mozilla.org/en/docs/Web/JavaScript">#js</a>
+              <a className="tag-link" href="https://developer.mozilla.org/en/docs/Web/JavaScript">#javascript</a>
+              <a className="tag-link" href="https://developer.mozilla.org/en/docs/Web/CSS">#css</a>
               <a className="tag-link" href="https://facebook.github.io/react/">#reactjs</a>
               <a className="tag-link" href="https://angular.io/">#angular</a>
               <a className="tag-link" href="http://php.net/">#php</a>
               <a className="tag-link" href="https://nodejs.org/">#nodejs</a>
               <a className="tag-link" href="https://www.mysql.com/">#mysql</a>
               <a className="tag-link" href="https://www.mongodb.com/">#mongodb</a>
-              <p className="bio">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.</p>
-              <p className="last-project">Lorem Ipsum es simplemente <a href="#">el texto</a> de relleno de las imprentas y archivos de texto. .</p>
-              <OverlayTrigger placement="top" overlay={tooltipGithub}>
-                <a className="social-link" href="#"><i className="fa fa-github right-10"></i></a>
-              </OverlayTrigger>
-              <OverlayTrigger placement="top" overlay={tooltipLinkedin}>
-                <a className="social-link" href="#"><i className="fa fa-linkedin right-10"></i></a>
-              </OverlayTrigger>
-              <OverlayTrigger placement="top" overlay={tooltipEmail}>
-                <a className="social-link" href="#"><i className="fa fa-envelope right-10"></i></a>
-              </OverlayTrigger>
-              <OverlayTrigger placement="top" overlay={tooltipSkype}>
-                <a className="social-link" href="#"><i className="fa fa-skype right-10"></i></a>
-              </OverlayTrigger>
+              <p className="bio">
+                I'm a software engineer based in Alicante (Spain) focused in build software following the best practices.
+                I've had the pleasure to work in/for several companies, from startups to big companies:&nbsp;
+                {/*this nbsp's are to put a space with no underline*/}
+                <a target="_blank" href="https://www.cymsuroeste.com/">CYM Suroeste</a>,&nbsp;
+                <a target="_blank" href="http://www.dual-link.com/">Dual Link</a>,&nbsp;
+                <a target="_blank" href="http://www.nitsnets.com/">Nitsnets</a>,&nbsp;
+                <a target="_blank" href="http://www.heartibiza.com/">HEART Ibiza</a>,&nbsp;
+                <a target="_blank" href="https://www.panamajack.es/">Panama Jack</a>,&nbsp;
+                <a target="_blank" href="http://www.mondraker.com/">Mondraker</a>,&nbsp;
+                <a target="_blank" href="http://blgabogados.es/">BLG Abogados</a>.&nbsp;
+              </p>
+              <p className="last-project">
+                Check out my last side project: <a target="_blank" href="http://elasticflight.com/">Elastic Flight</a> — Find cheap flights with no particular date.
+              </p>
+              <p className="social-links">
+                <OverlayTrigger placement="top" overlay={tooltipGithub}>
+                  <a className="social-link" target="_blank" href="https://github.com/jonathanmartinez"><i className="fa fa-github right-10"></i></a>
+                </OverlayTrigger>
+                <OverlayTrigger placement="top" overlay={tooltipLinkedin}>
+                  <a className="social-link" target="_blank" href="http://linkedin.com/in/jonathan-martinez1"><i className="fa fa-linkedin right-10"></i></a>
+                </OverlayTrigger>
+                <OverlayTrigger placement="top" overlay={tooltipEmail}>
+                  <a className="social-link" target="_blank" href="mailto:jonymartinez1@gmail.com"><i className="fa fa-envelope right-10"></i></a>
+                </OverlayTrigger>
+                <OverlayTrigger placement="top" overlay={tooltipSkype}>
+                  <a className="social-link" target="_blank" href="#"><i className="fa fa-skype right-10"></i></a>
+                </OverlayTrigger>
+              </p>
             </article>
           </section>
         </main>
